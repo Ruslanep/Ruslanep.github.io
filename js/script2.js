5c173dde666295c076f4db2fc72a8432390c3fc2
Ruslan Epishin <ruslanep@gmail.com>
2018-10-23 01:43:57 +0300

    add js to main-page

---
+++ js/script2.js
@@ -0,0 +1,30 @@
+(function(){
+    'use strict';
+
+    var btnScrollDown = document.querySelector('.button-slide-down');
+  
+    function scrollDown() {
+      var windowCoords = document.documentElement.clientHeight;
+      
+      (function scroll() {
+        if (window.pageYOffset < windowCoords) {
+          window.scrollBy(0, 15);
+          setTimeout(scroll, 0);
+        }
+        if (window.pageYOffset > windowCoords) {
+          window.scrollTo(0, windowCoords);
+        }
+      })();
+    }
+  
+    btnScrollDown.addEventListener('click', scrollDown);
+
+
+   
+
+    
+})();
+
+
+
+
