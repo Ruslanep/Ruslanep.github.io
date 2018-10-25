c67885496bc2df2d5c2d25792baae83d81c6fbe6
Ruslan Epishin <ruslanep@gmail.com>
2018-10-25 15:19:10 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,45 @@
 (function(){
     'use strict';
 
-  
+    var mainButton = document.querySelector('#main');
+    var aboutButton = document.querySelector('#about');
+
+    mainButton.addEventListener('click', function(){
+      mainButton.classList.add('active');
+      aboutButton.classList.remove('active');
+    });
+    aboutButton.addEventListener('click', function(){
+      aboutButton.classList.add('active');
+      mainButton.classList.remove('active'); 
+    });
+
+
+     ////////
+    ////////
+    
+
+    $(document).ready(function(){
+        $(".page-header__nav-wrapper").on("click","a", function (event) {
+            //отменяем стандартную обработку нажатия по ссылке
+            event.preventDefault();
+            //забираем идентификатор бока с атрибута href
+            var id  = $(this).attr('href'),
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+                top = $(id).offset().top;     
+            //анимируем переход на расстояние - top за 1500 мс
+            $('body,html').animate({scrollTop: top}, 500);
+        });
+     });
+    
+    
+
+    
+
+    
+
+    ////////
+    ////////
+    
 
     window.onscroll = function() {scrollFunction()};
 
@@ -25,7 +63,11 @@
 
     }
 
+    ////////
+    ////////
+
     
+           
 })();
 
 
